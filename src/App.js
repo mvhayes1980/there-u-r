abcfc3b936b424f054a4eb9e16a028647d928001
Tim <timculpcreations@gmail.com>
2020-05-20 00:01:59 -0400

    fixed fetch delay

--- src/App.js
+++ src/App.js
@@ -9,27 +9,31 @@ export default class App extends React.Component {
   constructor(props) {
       super(props);
       this.state = {
-        lat: 0,
-        lon: 0
+        lat: null,
+        lon: null
       }
   }
   componentWillMount() {
       navigator.geolocation.getCurrentPosition((position)=>{
         console.log("look here");
-        this.setState({lat: position.coords.latitude, lon: position.coords.longitude});
-        this.forceUpdate();
+        this.setState({
+          lat: position.coords.latitude, 
+          lon: position.coords.longitude
+        });
       })
   }
   render(){
     return (
+      this.state.lat != null ?
       <div className="App">
         <Navbar/>
         <OpenWeather lat={this.state.lat} lon={this.state.lon}/>
         <div style={{width: "400px"}}>
-        <NASAFetch lat={this.state.lat} lon={this.state.lon}/>
-        <Zomato lat={this.state.lat} lon={this.state.lon}/>
+          <NASAFetch lat={this.state.lat} lon={this.state.lon}/>
         </div>
+        <Zomato lat={this.state.lat} lon={this.state.lon}/>
       </div>
+      : null
     );
   }
 }
