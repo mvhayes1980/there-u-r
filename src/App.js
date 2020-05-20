7944f8ce4129d913f1f2e006fff69f4b182b549e
Jerad Lollar <jeradlollar@gmail.com>
2020-05-19 21:05:08 -0400

    jerad's branch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      You are HERE
-      DEVELOP BRANCH
+      <Navbar/>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import NavLogo from '../assets/logo.png';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    fontWeight: 800,
+    color: "white",
+  },
+}));
+
+export default function Navbar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar className="MuiAppBar" position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          <img id="navLogo" src={NavLogo} alt="logo" height='100px'/>
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            -INFO BASED ON YOUR LOCATION-
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
